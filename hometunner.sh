13b312d84d3bd9adb371c0a057b49173301c9aab
Mike Helmick <mike.helmick@gmail.com>
2006-05-18 14:30:42 +0000

    added ldap tunnel

--- hometunner.sh
+++ hometunner.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ssh -fNL 4000:uml3.sanclass.org:3306 helmicmt@work.mikehelmick.com
+ssh -fNL 4000:uml3.sanclass.org:3306 helmicmt@easlnx01.eas.muohio.edu
+ssh -fNL 4000:ldapsun1.muohio.edu:636 helmicmt@easlnx01.eas.muohio.edu
