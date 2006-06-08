48c37d2d0588d944347f3dee309472b05ec42ff7
Mike Helmick <mike.helmick@gmail.com>
2006-06-08 02:32:56 +0000

    download function added

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -22,6 +22,26 @@ class AssignmentsController < ApplicationController
     set_title
   end
 
+  def download
+    return unless load_course( params[:course] )
+    return unless allowed_to_see_course( @course, @user )
+    
+    @assignment = Assignment.find(params[:id]) rescue @assignment = Assignment.new
+    return unless assignment_in_course( @assignment, @course )
+    return unless assignment_available( @assignment )
+    
+    @document = AssignmentDocument.find(params[:document]) rescue @document = AssignmentDocument.new
+    return unless document_in_assignment( @document, @assignment )
+       
+       
+    begin  
+      send_file @document.resolve_file_name(@app['external_dir']), :filename => @document.filename, :type => "#{@document.content_type}", :disposition => 'inline'  
+    rescue
+      flash[:badnotice] = "Sorry - the requested document has been deleted or is corrupt.  Please notify your instructor of the problem and mention 'assignment document id #{@document.id}'."
+      redirect_to :action => 'view', :assignment => @assignment, :course => @course
+    end 
+  end
+
   def assignment_available( assignment )
     unless assignment.open_date <= Time.now
       flash[:badnotice] = "The requisted assignment is not yet available."
@@ -39,6 +59,15 @@ class AssignmentsController < ApplicationController
     end
     true
   end
+  
+  def document_in_assignment( document, assignment )
+    unless document.assignment_id == assignment.id 
+      flash[:badnotice] = "The requested document could not be found."
+      redirect_to :action => 'view', :assignment => assignment, :course => @course
+      return false
+    end
+    true
+  end
 
   def set_tab
     @show_course_tabs = true
@@ -51,6 +80,6 @@ class AssignmentsController < ApplicationController
     @title = "#{@assignment.title} - #{@course.title}" unless @assignment.nil?
   end
   
-  private :set_tab, :set_title, :assignment_in_course, :assignment_available
+  private :set_tab, :set_title, :assignment_in_course, :assignment_available, :document_in_assignment
   
 end
