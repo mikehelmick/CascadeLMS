22ee31f5bdfcf394ad4c6e1bb91cad6370cfb075
Mike Helmick <mike.helmick@gmail.com>
2007-08-16 14:16:09 +0000

    remove disable account flag

--- db/migrate/075_add_account_disabled_flag.rb
+++
@@ -1,9 +0,0 @@
-class AddAccountDisabledFlag < ActiveRecord::Migration
-  def self.up  
-    add_column( :users, :enabled, :boolean, :null => false, :default => true )
-  end
-
-  def self.down
-    remove_column( :users, :enabled )
-  end
-end
