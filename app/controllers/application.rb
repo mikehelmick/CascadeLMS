65cbf0a247bc13ebc2e728b9a3d816dfefc92ab6
Mike Helmick <mike.helmick@gmail.com>
2007-09-09 14:06:27 +0000

    wiki page fix

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '0.8.11 (Paperweight) 20070905'
+  @@VERSION = '0.8.12 (Paperweight) 20070909'
   
   layout 'application' rescue puts "couldn't load default layout"
   
