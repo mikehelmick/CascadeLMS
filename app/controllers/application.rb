3ed7d97d2053890962a9165b3d0f751bdfb68700
Mike Helmick <mike.helmick@gmail.com>
2009-01-02 05:45:06 +0000

    fix the finalize problem with grants

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.0.3 (To The Light) 20090101'
+  @@VERSION = '1.0.4 (To The Light) 20090101'
   
   ## Supress password logging
   filter_parameter_logging :password
