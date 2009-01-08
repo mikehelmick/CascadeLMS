58648ec8525d8f1275df706eafaad83b2ce9a243
Mike Helmick <mike.helmick@gmail.com>
2009-01-08 16:11:44 +0000

    allow more than one instructor affilitation type

--- lib/LdapAuthentication.rb
+++ lib/LdapAuthentication.rb
@@ -102,9 +102,14 @@ class LdapAuthentication
     user.instructor = false
     user.activated = true
     user.affiliation = page[0][@settings['ldap_field_affiliation']].join(', ')
+
+    inst_affiliations = @settings['instructor_affiliation'].split(',')
+
     page[0][@settings['ldap_field_affiliation']].each do |x|
-      if x.downcase.eql?( @settings['instructor_affiliation'].downcase ) 
-        user.instructor = true
+      inst_affiliations.each do |instructor_affiliation|
+        if x.downcase.eql?( instructor_affiliation.downcase ) 
+          user.instructor = true
+        end
       end
     end
     user.personal_title = page[0][@settings['ldap_field_personaltitle']][0] unless page[0][@settings['ldap_field_personaltitle']].nil?
