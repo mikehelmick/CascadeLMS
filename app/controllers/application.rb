27f3ea8537be36a6236430e86b3d6986fb4a43c4
Mike Helmick <mike.helmick@gmail.com>
2009-01-08 03:51:38 +0000

    try and eliminate error conditions due to duplicate named parameters

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.0.9 (To The Light) 20090107'
+  @@VERSION = '1.0.10 (To The Light) 20090107'
   
   ## Supress password logging
   filter_parameter_logging :password
