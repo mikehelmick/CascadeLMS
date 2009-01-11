773a55c695d023f06270975589cc249f2a99214a
Mike Helmick <mike.helmick@gmail.com>
2009-01-11 17:52:17 +0000

    fix syntax error in forums/edit template

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.0.11 (To The Light) 20090108'
+  @@VERSION = '1.0.12 (To The Light) 20090111'
   
   ## Supress password logging
   filter_parameter_logging :password
