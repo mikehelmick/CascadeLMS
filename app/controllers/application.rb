ac900c76276bf1824ef98be2ee7f8ff633d7beb2
Mike Helmick <mike.helmick@gmail.com>
2009-01-15 06:41:26 +0000

    change links on upload / manage submitted files

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.0.24 (To The Light) 20090114'
+  @@VERSION = '1.0.25 (To The Light) 20090115'
   
   ## Supress password logging
   filter_parameter_logging :password
