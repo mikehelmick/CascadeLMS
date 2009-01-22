48f4a311eb3002faf41af6f2df8b8b12459c029b
Mike Helmick <mike.helmick@gmail.com>
2009-01-22 05:48:51 +0000

    fix incorrect controller errors

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.0.31 (To The Light) 20090120'
+  @@VERSION = '1.0.32 (To The Light) 20090121'
   
   ## Supress password logging
   filter_parameter_logging :password
