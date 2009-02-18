1938bca73c7e3bf5f6630e371121ff282539932b
Mike Helmick <mike.helmick@gmail.com>
2009-02-18 02:09:14 +0000

    don't show auto added files after AG

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.1.12 (Sing) 20090214'
+  @@VERSION = '1.1.13 (Sing) 20090217'
   
   ## Supress password logging
   filter_parameter_logging :password
