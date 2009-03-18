7e68b8108b0c4e6604a8d764bc466cf2f33efb8a
Mike Helmick <mike.helmick@gmail.com>
2009-03-18 19:47:00 +0000

    rename rubrics to excellent/proficient/apprentice/novice

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.2.0 (Om) 20090317'
+  @@VERSION = '1.2.1 (Om) 20090318'
   
   ## Supress password logging
   filter_parameter_logging :password
