8d83c6e57531fdef12628812ba19f6493a580ebc
Mike Helmick <mike.helmick@gmail.com>
2009-04-01 15:57:38 +0000

    fix code display for multiple select questions on quiz

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -29,7 +29,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.2.3 (Om) 20090326'
+  @@VERSION = '1.2.4 (Om) 20090401'
   
   ## Supress password logging
   filter_parameter_logging :password
