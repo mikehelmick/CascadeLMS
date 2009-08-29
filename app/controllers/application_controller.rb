41c37abeeb9045b57b38a75c3f982fd4ccc26267
Mike Helmick <mike.helmick@gmail.com>
2009-08-28 18:52:48 -0700

    correct ordering for rubrics [#25 state:resolved]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.3.11 (Rainier) 20090825'
+  @@VERSION = '1.3.12 (Rainier) 20090828'
   
   ## Supress password logging
   filter_parameter_logging :password
