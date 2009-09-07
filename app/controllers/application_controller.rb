09214da93dd5631bb7ef14e991015f46968ab6fc
Mike Helmick <mike.helmick@gmail.com>
2009-09-06 22:25:53 -0700

    fix jump to course when on a no_right instructor screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.4.2 (Rainier) 20090906'
+  @@VERSION = '1.4.3 (Rainier) 20090906'
   
   ## Supress password logging
   filter_parameter_logging :password
