d5c91a9cb52164e9ae33e83f31c7b76c5be96023
Mike Helmick <mike.helmick@gmail.com>
2009-10-15 08:14:02 -0700

    don't show hidden files in public view [#51 state:resolved]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.4.20 (Rainier) 20091014'
+  @@VERSION = '1.4.21 (Rainier) 20091015'
   
   ## Supress password logging
   filter_parameter_logging :password
