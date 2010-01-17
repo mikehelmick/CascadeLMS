e9af147bda8246f4891bb9fa9de892013c69bf34
Mike Helmick <mike.helmick@gmail.com>
2010-01-16 17:34:01 -0800

    re-enable programming languages definition, clean up view_student page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.4.30 (Rainier) 20100108'
+  @@VERSION = '1.4.31 (Rainier) 20100116'
   
   ## Supress password logging
   filter_parameter_logging :password
