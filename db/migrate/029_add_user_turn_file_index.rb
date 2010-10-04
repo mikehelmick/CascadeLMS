e3e226c5e09caef4618dab8d6d59eca1a26a33e4
toby cabot <ccabot@mitre.org>
2010-10-04 12:15:50 -0400

    change index names in migrations from symbols to strings
    Evidently this works with mysql but with sqlite you get an error when
    the code tries to see whether the index name is too long.  Symbols
    don't have a length() method so the migration crashes.  Strings work
    fine in all cases.

--- db/migrate/029_add_user_turn_file_index.rb
+++ db/migrate/029_add_user_turn_file_index.rb
@@ -1,6 +1,6 @@
 class AddUserTurnFileIndex < ActiveRecord::Migration
   def self.up
-     add_index(:user_turnin_files, [:user_turnin_id, :filename, :directory_parent], :name => :unique_filename_idx, :unique => true)
+     add_index(:user_turnin_files, [:user_turnin_id, :filename, :directory_parent], :name => 'unique_filename_idx', :unique => true)
   end
 
   def self.down
--- db/migrate/20100320214332_add_grade_category_index.rb
+++ db/migrate/20100320214332_add_grade_category_index.rb
@@ -1,6 +1,6 @@
 class AddGradeCategoryIndex < ActiveRecord::Migration
   def self.up
-    add_index(:grade_categories, [:course_id], :unique => false, :name => :grade_category_course_idx)
+    add_index(:grade_categories, [:course_id], :unique => false, :name => 'grade_category_course_idx')
   end
 
   def self.down
