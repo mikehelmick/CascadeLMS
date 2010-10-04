443457ba71f71a8a6cdfa5925a1b038b15a18656
toby cabot <ccabot@mitre.org>
2010-10-04 14:58:03 -0400

    genericized the default email settings

--- config/environment.rb
+++ config/environment.rb
@@ -55,12 +55,11 @@ end
 
 # Include your application configuration below
 
-# Include your application configuration below
 ActionMailer::Base.smtp_settings = {
-  :address  => 'mailfwd.muohio.edu',
-  :port  => 25, 
-  :domain  => 'muohio.edu'
-    }
+  :address  => 'localhost',
+  :port  => 8025,
+  :domain  => 'localhost.localdomain'
+}
 
 my_formats = {
   :friendly_date => '%A %B %d, %Y %I:%M:%S %p %Z',
