2a8bf1ca02f9b53f209c395047c2a1fe80c35cb6
Mike Helmick <mike.helmick@gmail.com>
2010-11-14 18:43:43 -0800

    update version after merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.4.58 (Rainier) 20101107'
+  @@VERSION = '1.4.59 (Rainier) 20101114'
   
   ## Supress password logging
   filter_parameter_logging :password
