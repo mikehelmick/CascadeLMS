5c5c39df2e3d800fc7f2f1a6ddf0d93bb4d517dd
Mike Helmick <mike.helmick@gmail.com>
2011-02-18 07:52:08 -0800

    Fix some bad link progressions in the auditor screens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.4.78 (Rainier) 20110214'
+  @@VERSION = '1.4.79 (Rainier) 20110218'
   
   ## Supress password logging
   filter_parameter_logging :password
