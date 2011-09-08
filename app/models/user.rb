b08492becbd1b46bde265b80132f8c0150e4a5b7
Mike Helmick <mike.helmick@gmail.com>
2011-09-07 22:15:50 -0700

    hide courses on the home screen that a student was dropped from

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User < ActiveRecord::Base
   
   ## Returns a courses_user obj - so you have the course and relationship to the course
   def courses_in_term( term )
-    cur = CoursesUser.find(:all, :conditions => ["user_id = ? and term_id =?", self.id, term.id])
+    cur = CoursesUser.find(:all, :conditions => ["user_id = ? and term_id =? and (course_student = ? or course_instructor = ? or course_guest = ? or course_assistant = ?)", self.id, term.id, true, true, true, true])
     cur.sort! do |x,y| 
       result = x.position <=> y.position
       result = x.course.title <=> y.course.title if result == 0
