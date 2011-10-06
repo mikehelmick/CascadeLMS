36604625a3cf2e7dd97ca1fb7b9b30ca6cca67cf
Mike Helmick <mike.helmick@gmail.com>
2011-10-06 08:32:08 -0700

    remove unnecessary columns from turnins overview screen. Realign/streamline data on this screen. [#141 state:resolved]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ require 'MyString'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '1.4.95 (Rainier) 20111005'
+  @@VERSION = '1.4.96 (Rainier) 20111006'
   
   ## Supress password logging
   filter_parameter_logging :password
