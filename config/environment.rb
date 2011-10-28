a29fdf4db93f8c6bc30d9f19386dd4304968c846
Mike Helmick <mike.helmick@gmail.com>
2011-10-27 21:30:52 -0700

    Move from using BackgroundJob as a plugin to as a gem

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'bj'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
