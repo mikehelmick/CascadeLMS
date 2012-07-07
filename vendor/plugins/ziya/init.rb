402f1f9aba1081ca6037d2c1f7cf22d75cd7f5fa
Mike Helmick <mike.helmick@gmail.com>
2012-07-07 10:10:04 -0700

    remove unused plugin

--- vendor/plugins/ziya/init.rb
+++
@@ -1,5 +0,0 @@
-require File.dirname(__FILE__) + '/lib/ziya'    
-require File.dirname(__FILE__) + '/lib/ziya_helper'    
-require File.dirname(__FILE__) + '/lib/ziya_charting'    
-
-ActionView::Base.send(:include, Ziya::Helper)       
