74e8929c19490dd36025382c0d419d7ca6747bf3
Mike Helmick <mike.helmick@gmail.com>
2013-03-09 13:21:27 -0800

    Update link to new issue tracker.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ require 'MyActiveRecordHelper'
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   ## CSCW Application version
-  @@VERSION = '2.0.029 <em>beta</em> (Jefferson) 20130307'
+  @@VERSION = '2.0.030 <em>beta</em> (Jefferson) 20130309'
   
   ## Supress password logging
   filter_parameter_logging :password
