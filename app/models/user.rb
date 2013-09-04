b7518ef2f0a23d4ec566d2c6ca3c26f83f24c954
Mike Helmick <mike.helmick@gmail.com>
2013-09-03 22:00:32 -0400

    Lower minimum userid length to 2 characters.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :message => 'That email address is already registered.'
   validates_presence_of :uniqueid, :password, :first_name, :last_name, :email
   validates_format_of :first_name, :last_name, :with => /^[a-zA-Z0-9 '-.][a-zA-Z0-9 '-.]*$/, :message => 'name may only contains letters, numbers, "\'", spaces and "-" characters.'
-  validates_length_of :uniqueid, :within => 4..99, :too_long => ' Your username must be less than 100 characters in length.', :too_short => 'Your username must be at least 4 characters in length.'
+  validates_length_of :uniqueid, :within => 2..99, :too_long => ' Your username must be less than 100 characters in length.', :too_short => 'Your username must be at least 2 characters in length.'
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   
   has_many :courses_users
